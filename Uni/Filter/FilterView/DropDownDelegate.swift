44364e2353a3e1023514b1da567d586a22ab8c07
Georg <stowaway000@yandex.ru>
2019-11-14 20:06:55 +0300

    #2 Add protocol for drop-down

---
+++ Uni/Filter/FilterView/DropDownDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  dropDownDelegate.swift
+//  university
+//
+//  Created by Георгий Куликов on 27.10.2019.
+//  Copyright © 2019 Георгий Куликов. All rights reserved.
+//
+
+import UIKit
+
+
+protocol DropDownDelegate {
+    var dropView: DropDownView { get set }
+    
+    func setUpDropView()
+    
+    func dropDownPressed(on title: String)
+    
+    func dismissDropDown()
+}

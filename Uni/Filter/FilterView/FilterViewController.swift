faf900afbc393c3d7f7d2e44fd1a4a4a0c7ead02
Georg <stowaway000@yandex.ru>
2019-11-14 19:53:14 +0300

    #2 Add view class for filter

---
+++ Uni/Filter/FilterView/FilterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  university
+//
+//  Created by Георгий Куликов on 11.10.2019.
+//  Copyright © 2019 Георгий Куликов. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

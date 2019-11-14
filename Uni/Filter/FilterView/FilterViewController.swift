f48242958ad5207bdcb7d11e098aac58c5958cd7
Georg <stowaway000@yandex.ru>
2019-11-14 20:01:33 +0300

    #2 Add views to view class

--- Uni/Filter/FilterView/FilterViewController.swift
+++ Uni/Filter/FilterView/FilterViewController.swift
@@ -18,6 +18,27 @@ class FilterViewController: UIViewController {
     private let dataSourceCountry = ["Москва", "Санкт-Петербург", "Омск", "Волгоград", "Владимир", "Екатеринбург", "Уфа", "Владивосток"]
     private let dataSourceSubject = ["Математика", "Русский", "Информатика", "Физика"]
     
+    private var contentView = UIView()
+    private var contentTable = UITableView()
+    
+    private let countryHeaderTitle: String = "Города"
+    private var countryButton = UIDropDownButton()
+    
+    private let subjectsHeaderTitle: String = "Предметы"
+    private var subjectsButton = UIDropDownButton()
+    
+    private var dataContentTable: [[UIDropDownButton]] = []
+    private var dataHeaderTitle: [String] = []
+    
+    private let pointsSlider = UISlider()
+    private let pointsTextField = UITextField()
+    
+    private let militaryLabel = UILabel()
+    private let militaryButton = UISwitch()
+    
+    private let campusLabel = UILabel()
+    private let campusButton = UISwitch()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }

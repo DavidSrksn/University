2905b4c8ff20ed901449750e1911a7297ea7bb94
Georg <stowaway000@yandex.ru>
2019-11-14 22:16:11 +0300

    #2 Update size

--- Uni/Filter/FilterModel/FilterSize.swift
+++ Uni/Filter/FilterModel/FilterSize.swift
@@ -12,30 +12,30 @@ import UIKit
 struct Size {
     let safeAreaBorder = CGFloat(20)
     
-    let contentViewY = CGFloat(180)
-    let contentViewHeight = CGFloat(180)
-    
     let countryButtonY = CGFloat(60)
-    let countryButtonHeight = CGFloat(45)
+    let countryButtonHeight = CGFloat(150)
+    
+    let contentViewY = CGFloat(220)
+    let contentViewHeight = CGFloat(220)
     
     let subjectsButtonY = CGFloat(120)
     let subjectsButtonHeight = CGFloat(45)
     
-    let pointsSliderY = CGFloat(180)
+    let pointsSliderY = CGFloat(40)
     let pointsSliderHeight = CGFloat(30)
     
-    let pointsTextFieldY = CGFloat(220)
+    let pointsTextFieldY = CGFloat(80)
     let pointsTextFieldHeight = CGFloat(30)
     
-    let militaryLabelY = CGFloat(270)
+    let militaryLabelY = CGFloat(130)
     let militaryLabelHeight = CGFloat(30)
     
-    let militaryButtonY = CGFloat(270)
+    let militaryButtonY = CGFloat(130)
     let militaryButtonHeight = CGFloat(45)
     
-    let campusLabelY = CGFloat(320)
+    let campusLabelY = CGFloat(180)
     let campusLabelHeight = CGFloat(30)
     
-    let campusButtonY = CGFloat(320)
+    let campusButtonY = CGFloat(180)
     let campusButtonHeight = CGFloat(45)
 }

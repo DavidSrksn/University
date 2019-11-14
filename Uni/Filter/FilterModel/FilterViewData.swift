a8201f8190188cde345016d8b7653f44f51b7795
Georg <stowaway000@yandex.ru>
2019-11-14 19:36:57 +0300

    #2 Add file with data for view settings

---
+++ Uni/Filter/FilterModel/FilterViewData.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+struct FilterViewData {
+    let FilterViewColor = UIColor.white
+    let FilterDropDownColor = UIColor.black
+    
+    let cornerRadius = CGFloat(10)
+    
+    let militaryButtonColor = UIColor.black
+    let campusButtonColor = UIColor.black
+}

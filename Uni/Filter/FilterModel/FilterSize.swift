9c535124af9eaa8366661938b1f71fd6f8eecbbb
Georg <stowaway000@yandex.ru>
2019-11-14 19:38:47 +0300

    #2 Add file with data for constraints

---
+++ Uni/Filter/FilterModel/FilterSize.swift
@@ -0,0 +1,41 @@
+//
+//  Size.swift
+//  university
+//
+//  Created by Георгий Куликов on 26.10.2019.
+//  Copyright © 2019 Георгий Куликов. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Size {
+    let safeAreaBorder = CGFloat(20)
+    
+    let contentViewY = CGFloat(180)
+    let contentViewHeight = CGFloat(180)
+    
+    let countryButtonY = CGFloat(60)
+    let countryButtonHeight = CGFloat(45)
+    
+    let subjectsButtonY = CGFloat(120)
+    let subjectsButtonHeight = CGFloat(45)
+    
+    let pointsSliderY = CGFloat(180)
+    let pointsSliderHeight = CGFloat(30)
+    
+    let pointsTextFieldY = CGFloat(220)
+    let pointsTextFieldHeight = CGFloat(30)
+    
+    let militaryLabelY = CGFloat(270)
+    let militaryLabelHeight = CGFloat(30)
+    
+    let militaryButtonY = CGFloat(270)
+    let militaryButtonHeight = CGFloat(45)
+    
+    let campusLabelY = CGFloat(320)
+    let campusLabelHeight = CGFloat(30)
+    
+    let campusButtonY = CGFloat(320)
+    let campusButtonHeight = CGFloat(45)
+}

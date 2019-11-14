4e3e5e460d5f3cadddca90534d31340f2609e3c8
Georg <stowaway000@yandex.ru>
2019-11-14 19:49:44 +0300

    #2 Add Presenter class with updating methods

---
+++ Uni/Filter/FilterPresenter/FilterPresenter.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+struct FilterPresenter {
+    private var model = Filter()
+    
+    init() {
+        model.country = []
+        model.subjects = []
+        model.minPoint = 100
+        model.military = true
+        model.campus = true
+    }
+    
+    mutating func addCountry(newCountrys: [String]) {
+        
+    }
+    
+    mutating func updateSubject(newSubjects: [String]) {
+        
+    }
+    
+    mutating func changeMinPoint(for value: Int) {
+        model.minPoint? = value
+    }
+    
+    mutating func changeMilitary(for value: Bool) {
+        model.military? = value
+    }
+    
+    mutating func changeCampus(for value: Bool) {
+        model.campus? = value
+    }
+    
+    func countOfCountrys() -> Int? {
+        return self.model.country?.count
+    }
+    
+    func countOfSubjects() -> Int? {
+        return self.model.subjects?.count
+    }
+}
